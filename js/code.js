4baf65c0e87e9819a6420da2491f73b0b4d9950e
Ahmed-Atya <ahmedatya@outlook.com>
2022-03-29 14:58:28 +0200

    all themes files

---
+++ js/code.js
@@ -0,0 +1,8 @@
+let toggleList = document.querySelector(".icon");
+let list = document.getElementById("list");
+
+toggleList.addEventListener("click",function(){
+
+ list.classList.toggle("open");
+});
+ 
